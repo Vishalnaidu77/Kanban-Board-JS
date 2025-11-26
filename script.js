adc1fef6474fdc5cfb9e7da43262b9d037317b0e
Vishal naidu <vminorr@gmail.com>
2025-11-26 18:59:28 +0000

    Build Add task feature.

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const progress = document.querySelector("#progress")
 const done = document.querySelector("#done")
 const tasks = document.querySelectorAll('.task')
 
-console.log(todo, progress, done);
+
+// console.log(todo, progress, done);
+
+let draggedItem = null;
 
 tasks.forEach((task) => {
     task.addEventListener("drag", (e) => {
         // console.log(e);
+        draggedItem = task;
     })
 })
 
@@ -16,14 +20,68 @@ function addDragEventOncolumn(cols){
     cols.addEventListener("dragenter", (e) => {
         e.preventDefault();
         cols.classList.add("hover-over")
+        // console.log(`Element enter in ${cols}`);
     })
 
     cols.addEventListener("dragleave", (e) => {
         e.preventDefault();
         cols.classList.remove("hover-over")
     })
+
+    cols.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
+
+    cols.addEventListener("drop", (e) => {
+        e.preventDefault()
+        // console.log(draggedItem);
+        cols.appendChild(draggedItem)
+        cols.classList.remove("hover-over")
+    })
+    
 }
+
+
+// Modal Logic
+
+const toggleModal = document.querySelector("#toggle-modal")
+const modal = document.querySelector(".modal")
+const modalBg = document.querySelector(".bg")
+const addNewTask = document.querySelector(".modal #add-new-task")
 
 addDragEventOncolumn(todo)
 addDragEventOncolumn(progress)
 addDragEventOncolumn(done)
+
+toggleModal.addEventListener("click", () => {
+    modal.classList.toggle("active")
+})
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active")
+})
+
+
+addNewTask.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title").value
+    const taskDesc = document.querySelector("#task-desc").value
+
+    const div = document.createElement("div")
+
+    div.classList.add("task")
+    div.setAttribute("draggable", "true")
+
+    div.innerHTML = `
+        <h2>${taskTitle}</h2>
+        <p>${taskDesc}</p>
+        <button>Delete</button>
+    `
+
+    todo.appendChild(div)
+
+    div.addEventListener("drag", (e) => {
+        draggedItem = div;
+    })
+
+    modal.classList.remove("active")
+})
